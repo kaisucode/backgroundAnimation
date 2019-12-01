14b81591ec4db1c2621d6d1268f966e99e6345db
Alek Westover <alek.westover@gmail.com>
2019-12-01 00:22:06 -0500

    minor asthetic improvements

--- sketch.js
+++ sketch.js
@@ -11,29 +11,31 @@
 
 let dots = [];
 let dotTrajectories = [];
-let n = 30;
+let redirectPr = 0.1;
+let wallEjectionSpeedMultiplier = 30;
+let n = 25;
 let neighborThresh = 3; // the max number of connections
-let distThresh = 200;
+let distThresh = 100;
 
 let speed = 1;
 
 function setup(){
 	createCanvas(500, 500);
-
 	for(let i=0; i < n; i++){
 		dots.push(createVector(random()*width, random()*height));
+    dotTrajectories.push(random()*TWO_PI);
 	}
-
 }
 
 function draw(){
+	background(200,200,200);
+  stroke(0,0,0,50);
+	perturbDots();
+  connectDots();
 	fill(0);
-	background(255);
 	for(let i in dots){
 		ellipse(dots[i].x, dots[i].y, 10, 10);
 	}
-	perturbDots();
-  connectDots();
 }
 
 function realMod(n, m){
@@ -43,23 +45,37 @@ function realMod(n, m){
 function perturbDots(){
 	for(let i in dots){
 
-		dots[i].x += randomGaussian(0, speed);
-		dots[i].y += randomGaussian(0, speed);
+    let dotHeading = p5.Vector.fromAngle(dotTrajectories[i]).mult(speed);
+    dots[i].add(dotHeading);
+
+    if (random() < redirectPr) {
+      dotTrajectories[i] += randomGaussian(0,TWO_PI/12);
+    }
+
+    // pretty good randomness
+		// dots[i].x += randomGaussian(0, speed);
+		// dots[i].y += randomGaussian(0, speed);
+   
+    // less good randomness
 		// dots[i].x += (random()-0.5)*10*speed;
 		// dots[i].y += (random()-0.5)*10*speed;
 
     // bounce off boundaries
     if (dots[i].x > width){
-      dots[i].x -= speed*5;
+      dots[i].x -= speed*wallEjectionSpeedMultiplier;
+      dotTrajectories[i] += PI;
     }
     else if (dots[i].x < 0){
-      dots[i].x += speed*5;
+      dots[i].x += speed*wallEjectionSpeedMultiplier;
+      dotTrajectories[i] += PI;
     }
     if (dots[i].y > height){
-      dots[i].y -= speed*5;
+      dots[i].y -= speed*wallEjectionSpeedMultiplier;
+      dotTrajectories[i] += PI;
     }
     else if (dots[i].y < 0){
-      dots[i].y += speed*5;
+      dots[i].y += speed*wallEjectionSpeedMultiplier;
+      dotTrajectories[i] += PI;
     }
 
 	}
